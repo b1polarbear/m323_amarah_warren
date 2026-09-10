556fb5b447746eab9652a606e87d0a957a5e96a1
b1polarbear <potato.24e@gmail.com>
2026-09-10 15:35:38 +0200

    format code

--- src/functionalprog/beginner/task1a.rs
+++ src/functionalprog/beginner/task1a.rs
@@ -6,14 +6,22 @@ pub fn celsius_to_fahrenheit(celsius: f64) -> f64 {
     (celsius * 9.0 / 5.0) + 32.0
 }
 
-// Impure function - accesses outside world (using console)
+// Impure function - accesses outside world
 pub fn greet_user(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
-// Test Pure function
+// Note: Impure functions can be impure by having side effects or accessing the outside world
+
+/*
+referential transparency
+let temperature = celsius_to_fahrenheit(0.0);
+let temperature = 32.0;
+*/
+
+// Test Pure function (easily testable with no external help)
 #[test]
 fn test_celsius_to_fahrenheit() {
-    assert_eq!(celsius_to_fahrenheit(0.0), 32.0);
+    assert_eq!(celsius_to_fahrenheit(0.0), 32.0); // 0° = 32F
     assert_eq!(celsius_to_fahrenheit(100.0), 212.0);
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use functionalprog::beginner::task1a::{celsius_to_fahrenheit, greet_user};
 
 fn main() {
     println!("{}", celsius_to_fahrenheit(0.0));
-    println!("{}", celsius_to_fahrenheit(0.0));
+    println!("{}", celsius_to_fahrenheit(100.0));
 
-    println!("{}", greet_user("Alice"));
     println!("{}", greet_user("Alice"));
 }
